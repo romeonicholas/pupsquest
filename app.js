b7b0c6f5e850b410e72d6a66d172152ac83c4fdc
romeonicholas <nicholas.romeo@code.berlin>
2025-08-01 16:55:16 +0200

    Add dotenv and move server data into it

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import express from "express";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "localhost";
 
 app.use(express.json({ limit: "10mb" }));
 
@@ -17,14 +22,6 @@ app.get("/", (request, response) => {
   response.render("index");
 });
 
-app.listen(PORT, "10.65.0.77", () => {
-  console.log(`👋 Started server on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`👋 Started server on ${HOST}:${PORT}`);
 });
-
-// app.listen(PORT, () => {
-//   console.log(`👋 Started server on port ${PORT}`);
-// });
-
-// app.listen(PORT, "192.168.68.111", () => {
-//   console.log(`👋 Started server on port ${PORT}`);
-// });
