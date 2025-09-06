a3b4d697b1112abf902f890d69e46b57c560ca3d
romeonicholas <nicholas.romeo@code.berlin>
2025-09-06 09:55:19 +0200

    Add placeholder animals to user animal seeder

--- db/seeds/userAnimals.js
+++ db/seeds/userAnimals.js
@@ -1,10 +1,35 @@
 export function seedUserAnimals(db) {
   const rows = [
-    ["Armadillo", "assets/images/userCreation/badge_armadillo.png"],
-    ["Fox", "assets/images/userCreation/badge_fox.png"],
-    ["Hummingbird", "assets/images/userCreation/badge_hummingbird.png"],
-    ["Porcupine", "assets/images/userCreation/badge_porcupine.png"],
-    ["Prairie Dog", "assets/images/userCreation/badge_prairie_dog.png"],
+    ["Armadillo", "assets/images/userCreation/badge_icon_armadillo.png"],
+    ["Hummingbird", "assets/images/userCreation/badge_icon_hummingbird.png"],
+    ["Porcupine", "assets/images/userCreation/badge_icon_porcupine.png"],
+    ["Prairie Dog", "assets/images/userCreation/badge_icon_prairie_dog.png"],
+
+    // Placeholder animals with same image for now
+    ["Raccoon", "assets/images/userCreation/badge_icon_armadillo.png"],
+    ["Red Fox", "assets/images/userCreation/badge_icon_armadillo.png"],
+    ["Skunk", "assets/images/userCreation/badge_icon_armadillo.png"],
+    ["Squirrel", "assets/images/userCreation/badge_icon_armadillo.png"],
+    ["Woodpecker", "assets/images/userCreation/badge_icon_armadillo.png"],
+    ["Blue Jay", "assets/images/userCreation/badge_icon_armadillo.png"],
+    ["Bobcat", "assets/images/userCreation/badge_icon_armadillo.png"],
+    [
+      "Cottontail Rabbit",
+      "assets/images/userCreation/badge_icon_armadillo.png",
+    ],
+    ["Deer", "assets/images/userCreation/badge_icon_armadillo.png"],
+    ["Great Horned Owl", "assets/images/userCreation/badge_icon_armadillo.png"],
+    ["Mourning Dove", "assets/images/userCreation/badge_icon_armadillo.png"],
+    ["Red-tailed Hawk", "assets/images/userCreation/badge_icon_armadillo.png"],
+    ["Turkey", "assets/images/userCreation/badge_icon_armadillo.png"],
+    ["Weasel", "assets/images/userCreation/badge_icon_armadillo.png"],
+    ["Wild Turkey", "assets/images/userCreation/badge_icon_armadillo.png"],
+    [
+      "Woodland Jumping Mouse",
+      "assets/images/userCreation/badge_icon_armadillo.png",
+    ],
+    ["Coyote", "assets/images/userCreation/badge_icon_armadillo.png"],
+    ["Eastern Chipmunk", "assets/images/userCreation/badge_icon_armadillo.png"],
   ];
 
   const tx = db.transaction((items) => {
